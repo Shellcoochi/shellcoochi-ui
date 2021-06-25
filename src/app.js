180adedb478e9fe52f692551eb5c13ffee8a813d
DESKTOP-J3F3SIU\supOS <11111111111111111111111@163.com>
2021-06-25 16:35:05 +0800

    feat: ceate button files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Button from '@component/button'
+
 import styles from "./app.module.less";
 
 const Main = (props) => (
