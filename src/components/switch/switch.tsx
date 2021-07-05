446e443a681cfe5e2be3a6654a827586de1b08f7
DESKTOP-J3F3SIU\supOS <11111111111111111111111@163.com>
2021-07-05 20:11:40 +0800

    feat: add switch function #switch

--- src/components/switch/switch.tsx
+++ src/components/switch/switch.tsx
@@ -2,25 +2,38 @@ import * as React from "react";
 import classNames from "classnames";
 
 import styles from "./style/index.module.less";
-
-declare const ButtonTypes: ["default", "dashed", "primary", "ghost"];
-declare type ButtonType = typeof ButtonTypes[number];
-declare const ButtonSizes: ["small", "default", "large"];
-declare type ButtonSize = typeof ButtonSizes[number];
-export interface BaseButtonProps {
-  children?: React.ReactNode;
-  type?: ButtonType;
-  size?: ButtonSize;
-  disabled?: boolean;
+export interface SwitchProps {
+  checked?: boolean;
   onClick?: React.MouseEventHandler<HTMLElement>;
 }
-class Switch extends React.Component<BaseButtonProps, {}> {
+
+export interface SwitchState {
+  checked?: boolean;
+}
+class Switch extends React.Component<SwitchProps, SwitchState> {
+  constructor(props: SwitchProps) {
+    super(props);
+    this.state = {
+      checked: this.props.checked,
+    };
+  }
+  handleSwitchChange = () => {
+    const { checked } = this.state;
+    this.setState({
+      checked: !checked,
+    });
+  };
   render() {
+    const { checked } = this.state;
     const classes = classNames(
       styles[`scc-switch`],
-      styles[`scc-switch-unchecked`]
+      styles[`scc-switch-${checked?'checked':'unchecked'}`]
     );
-    return <button className={classes}><span>switch</span></button>;
+    return (
+      <button onClick={this.handleSwitchChange} className={classes}>
+        <span>switch</span>
+      </button>
+    );
   }
 }
 
